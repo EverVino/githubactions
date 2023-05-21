1982bbe1f68ff1613743d504b49b50f74affb1ed
EverVino <evervino00@gmail.com>
2023-05-21 00:24:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-def saludos(name):
-    print(f"saludos {name}")
-
-    
 def despedida(name):
     print(f"Hasta luego {name}")
 
-    
+
+def saludos(name):
+    print(f"saludos {name}")
+
+
 saludos("Ever")
 
 despedida("John")
