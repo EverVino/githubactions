a46e1505cec66c195dc266f1c4b076b99763a355
EverVino <evervino00@gmail.com>
2023-05-21 02:13:11 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from main import saludos, despedida
 
 saludo = saludos("Ever")
+print(saludo)
 assert saludo == "saludos Ever"
 desp = despedida("John")
+print(desp)
 assert desp == "Hasta luego John"
